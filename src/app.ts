9b0b544df84a8af809c8a0e60dbac57a44c304e7
mac@Dev <christmoses1999@gmail.com>
2023-03-17 22:52:06 +0100

    working on express

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import {
     Request,
     Response,
     NextFunction,
-    Application,
  } from "express";
 import bodyParser from "body-parser";
 
@@ -12,10 +11,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/"), (req: Request, res: Response, nest: NextFunction) => {
-    res.send("Hell my friend");
-};
+app.get("/", (req: Request, res: Response, nest: NextFunction) => {
+    res.send("Hello my friends!");
+});
 
 app.listen(port, ()=> {
     console.log(`server runing on port ${port}`);

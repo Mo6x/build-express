19f49f668b757b4b56ce06db3d9bcd239e9dea74
mac@Dev <christmoses1999@gmail.com>
2023-03-19 18:47:29 +0100

    working on express

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,14 @@ import {
     Response,
     NextFunction,
  } from "express";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req: Request, res: Response, nest: NextFunction) => {
     res.send("Hello my friends!");

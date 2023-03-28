ac26030f4566d7d8ac4870b7de2386e3ba82947d
mac@Dev <christmoses1999@gmail.com>
2023-03-28 11:02:45 +0100

    added the form list of the book title, desc, price, cover

--- Clients/my-react-app/src/Pages/Add.jsx
+++ Clients/my-react-app/src/Pages/Add.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
 
 
 const Add = () => {
+
   return (
-    <div>Add</div>
+    <div className="form">
+        <h1>Add New Book</h1>
+        <input type="text" placeholder="title" />
+        <input type="text" placeholder="desc" />
+        <input type="number" placeholder="price" />
+        <input type="text" placeholder="cover" />
+    </div>
   )
 }
 
-export default Add
+export default Add;
